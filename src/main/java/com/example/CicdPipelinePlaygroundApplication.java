7529b47b2550a6488bd6c7097797fde4ab5fc5c9
aransword <lupin521@gmail.com>
2026-04-14 11:12:23 +0900

    first commit

---
+++ src/main/java/com/example/CicdPipelinePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPipelinePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPipelinePlaygroundApplication.class, args);
+	}
+
+}
